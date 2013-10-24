c2cff45449da25764f4dc7b19db436cf6fcb6dce
E.Azer Koçulu <azer@kodfabrik.com>
2013-10-23 20:43:42 -0700

    new stuff

--- config.bash
+++ config.bash
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$PATH:$DIR
 
-source ~/.local/bin/bashmarks/bashmarks.sh
-source ~/.local/bin/git-completion.bash
+source ~/localbin/bashmarks/bashmarks.sh
+source ~/localbin/git-completion.bash
